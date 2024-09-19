ef0132044b0eda74e0533a729c939e95ff656e9f
Joel <yowismr@gmail.com>
2024-09-19 13:07:23 +0100

    Upload Script
    Hacer que las imagenes del cartel aparezca

--- Project1/script.js
+++ Project1/script.js
@@ -1,8 +1,3 @@
-var divId = document.createElement("div");
-
-var img = document.createElement("img");
-
-var divPrinc = document.getElementById("imgCartel")
 
 
 
@@ -10,22 +5,42 @@ var divPrinc = document.getElementById("imgCartel")
 /* CARTELES */
 var cartel = parseInt(prompt("Escriba el número del carteles que busca por favor"));
 
-for (let i  = 0; cartel > 0; i++){
-    if (cartel == null){
-        alert("Debes introducir una cantidad mayor de 0");
-    } else {
-       img.src = "images/WORTEN.png"
+if (cartel == null || cartel <= 0){
+    alert("Debes introducir una cantidad mayor de 0");
+} else{
+    for (let i  = 0; i < cartel; i++){
+        document.write(`<div>`);
+        document.write(`<img src = "images/WORTEN.png">`);
+        document.write(`</div>`);
     }
 }
 
+
+
+
 /* NUMEROS Y PUERTAS */
 
-var numPuertas = parseInt(prompt ("Escribe el numero de puerta "));
+var numPuertas = parseInt(prompt ("Escribe el numero de puerta"));
 numPuertas +=2;
 
 var puertas = parseInt(prompt("Di el numero de puertas que habrá"))
 
+
+
 /* ESCAPARATES Y OFERTAS */
+var ofertas = parseInt(prompt("Escribe que descuento quieres"));
+print(ofertas);
+
+var escaparates = parseInt ("Cuantos escaparates hay");
+for (let i  = 0; escaparates > 0; i++){
+    if (escaparates == null){
+        alert("Debes introducir una cantidad mayor de 0");
+    } else {
+       img.src = "images/Tienda.png"
+    }
+}
+
+
 
 
 
@@ -34,3 +49,9 @@ var puertas = parseInt(prompt("Di el numero de puertas que habrá"))
 
 
 /* COCHES */
+
+
+
+
+
+
