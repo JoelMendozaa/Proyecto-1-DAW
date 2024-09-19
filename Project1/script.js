ee0c8fe5bf080e1c41333e31112b9d7a7ebf54c4
Yowi <114146596+JoelMendozaa@users.noreply.github.com>
2024-09-19 18:53:33 +0200

    Update script
    Se completó el apartado de escaparates y ofertas

--- Project1/script.js
+++ Project1/script.js
@@ -8,11 +8,11 @@ var cartel = parseInt(prompt("Escriba el número del carteles que busca por favo
 if (cartel == null || cartel <= 0){        // Si el numero escrito por teclado es nulo o menor o igual a 0, lanza alerta
     alert("Debes introducir una cantidad mayor de 0");
 } else{
+    document.write(`<div class="images">`);
     for (let i  = 0; i < cartel; i++){          // Sino, muestra las imagenes de los cartes tantas veces se haya escrito por teclado
-        document.write(`<div class="images">`);
         document.write(`<img src = "images/WORTEN.png">`);
-        document.write(`</div>`);
     }
+    document.write(`</div>`);
 }
 
 
@@ -20,24 +20,23 @@ if (cartel == null || cartel <= 0){        // Si el numero escrito por teclado e
 
 var numPuertas = parseInt(prompt ("Escribe el numero de puerta por el que se inicia"));     // A través de un prompt, se pide un numero entero
 
-if (numPuertas == null ||numPuertas <= 0){
+if (numPuertas == null ||numPuertas <= 0){      // Si el numero introducido es menor o igual a 0 o en su defecto, no se ha colocado ninguno, saldrá una alerta
     alert("Debes colocar un número válido");
 } else {
-    var puertas = parseInt (prompt("Di el número de puertas que habrá"));
+    var puertas = parseInt (prompt("Di el número de puertas que habrá"));       // A través de un prompt, se pide un numero entero
 }
 
 if (puertas == null || puertas <= 0){        // Si el numero escrito por teclado es nulo o menor o igual a 0, lanza alerta
     alert("Debes introducir una cantidad mayor de 0");
 } else{
     document.write(`<div class = "images">`);
-
-    for (let i = 0; i < puertas; i++){
-        document.write(`<div class="display">`);
-        document.write(`<h4>${numPuertas}</h4>`);
+    for (let i = 0; i < puertas; i++){          // Por cada puerta que aparece según el numero introducido
+        document.write(`<div>`);
+        document.write(`<h4>${numPuertas}</h4>`);       // Aparecen el numero de las puertas
         document.write(`<img src = "images/Puerta.png">`);
         document.write(`</div>`);
 
-        numPuertas += 2;
+        numPuertas += 2;        // Sin importar el número que sea, se le sumará +2 para que este aumente según la puerta
     }
 
     document.write(`</div>`);
@@ -45,19 +44,38 @@ if (puertas == null || puertas <= 0){        // Si el numero escrito por teclado
 
 /* ESCAPARATES Y OFERTAS */
 
-var escaparates = parseInt(prompt("Cuantos escaparates hay"));
-if (escaparates == null || escaparates <= 0){        // Si el numero escrito por teclado es nulo o menor o igual a 0, lanza alerta
+var ofertas = parseInt(prompt("Escribe que descuento quieres"));
+if (ofertas == null || ofertas <= 0){
+    alert ("Por favor, coloca un número para saber el descuento deseado")
+} else {
+    var escaparates = parseInt(prompt("Cuantos escaparates hay"));
+}
+if (escaparates == null || escaparates <= 0){  
     alert("Debes introducir una cantidad mayor de 0");
 } else{
-    for (let i  = 0; i < escaparates; i++){          // Sino, muestra las imagenes de los escaparates/tiendas tantas veces se haya escrito por teclado
-        document.write(`<div class="display">`);
+    document.write(`<div class = "images">`);
+    for (let i = 0; i < escaparates; i++){          // Por cada puerta que aparece según el numero introducido
+        document.write(`<div>`);
+        document.write(`<h4>${ofertas}</h4>`);       // Aparecen el numero de las puertas
         document.write(`<img src = "images/Tienda.png">`);
         document.write(`</div>`);
     }
+    document.write(`</div>`);
 }
 
-var ofertas = parseInt(prompt("Escribe que descuento quieres"));
-print(ofertas);
+/*if (escaparates == null || escaparates <= 0){        // Si el numero escrito por teclado es nulo o menor o igual a 0, lanza alerta
+    alert("Debes introducir una cantidad mayor de 0");
+} else{
+    document.write(`<div class = "images">`);
+    for (let i  = 0; i < escaparates; i++){          // Sino, muestra las imagenes de los escaparates/tiendas tantas veces se haya escrito por teclado
+        document.write(`<div class="images">`);
+        document.write(`<img src = "images/Tienda.png">`);
+        document.write(`</div>`);
+    }
+    document.write(`</div>`);
+}*/
+
+
 
 /* SEMAFOROS Y RELOJ */
 
@@ -90,7 +108,7 @@ if (hora == null || hora <= 0 || hora > 24){       // Si la hora esrita es menor
 
         case 12 || 24:
 
-        
+
     }
 }
 
