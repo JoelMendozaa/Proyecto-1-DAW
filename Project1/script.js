6a224a6c63992206978189a0e1c2cd67a65c832a
Joel <yowismr@gmail.com>
2024-09-23 09:17:48 +0100

    Update index, style and script
    Añadir linea de texto en index
    Mejorar style para images
    Arreglar reloj y semaforo en script

--- Project1/script.js
+++ Project1/script.js
@@ -2,7 +2,7 @@
 /* CARTELES */
 var cartel = parseInt(prompt("Escriba el número del carteles que busca por favor"));
 
-if (cartel == null || cartel <= 0){        // Si el numero escrito por teclado es nulo o menor o igual a 0, lanza alerta
+if (cartel <= 0){        // Si el numero escrito por teclado es nulo o menor o igual a 0, lanza alerta
     alert("Debes introducir una cantidad mayor de 0");
 } else{
     document.write(`<div class="images">`);
@@ -17,7 +17,7 @@ if (cartel == null || cartel <= 0){        // Si el numero escrito por teclado e
 
 var numPuertas = parseInt(prompt ("Escribe el numero de puerta por el que se inicia"));     // A través de un prompt, se pide un numero entero
 
-if (numPuertas == null ||numPuertas <= 0){      // Si el numero introducido es menor o igual a 0 o en su defecto, no se ha colocado ninguno, saldrá una alerta
+if (numPuertas <= 0){      // Si el numero introducido es menor o igual a 0 o en su defecto, no se ha colocado ninguno, saldrá una alerta
     alert("Debes colocar un número válido");
 } else {
     var puertas = parseInt (prompt("Di el número de puertas que habrá"));       // A través de un prompt, se pide un numero entero
@@ -42,12 +42,12 @@ if (puertas == null || puertas <= 0){        // Si el numero escrito por teclado
 /* ESCAPARATES Y OFERTAS */
 
 var ofertas = parseInt(prompt("Escribe que descuento quieres"));
-if (ofertas == null || ofertas <= 0){
-    alert ("Por favor, coloca un número para saber el descuento deseado")
+if (ofertas <= 0){
+    alert ("Por favor, coloca un número para saber el descuento deseado");
 } else {
     var escaparates = parseInt(prompt("Cuantos escaparates hay"));
 }
-if (escaparates == null || escaparates <= 0){  
+if (escaparates <= 0){  
     alert("Debes introducir una cantidad mayor de 0");
 } else{
     document.write(`<div class = "images">`);
@@ -62,62 +62,81 @@ if (escaparates == null || escaparates <= 0){
 
 
 /* SEMAFOROS Y RELOJ */
-
-var hora = parseInt(prompt("¿Qué hora es?"))
-if (hora == null || hora <= 0 || hora > 24){       // Si la hora esrita es menor o igual que 0 o es superior a 24 o no se pone, lanza alerta
+document.write(`<div class="images">`);
+var hora = parseInt(prompt("¿Qué hora es?"));
+if (hora <= 0 || hora > 24){       // Si la hora esrita es menor o igual que 0 o es superior a 24 o no se pone, lanza alerta
     alert("Esa hora no existe");
 } else {
-    switch (hora){
+    switch (hora){         // Dependiendo del numero escrito entre 1 y 24, aparecerá una imagen distinta
         case 1:
         case 13:
+            document.write(`<img src="images/Reloj1.jpg"`);
         break;
-
         case 2:
         case 14:
+            document.write(`<img src="images/Reloj2.jpg"`);
         break;
-
         case 3:
         case 15:
+            document.write(`<img src="images/Reloj3.jpg"`);
         break;
-
         case 4: 
-        case 16: 
+        case 16:
+            document.write(`<img src="images/Reloj4.jpg"`);
         break;
-
         case 5: 
         case 17:
+            document.write(`<img src="images/Reloj5.jpg"`);
         break;
-
         case 6:
         case 18:
+            document.write(`<img src="images/Reloj6.jpg"`);
         break;
-
         case 7:
         case 19:
+            document.write(`<img src="images/Reloj7.jpg"`);
         break;
-
         case 8:
-        case 20: 
+        case 20:
+            document.write(`<img src="images/Reloj8.jpg"`);
         break;
-
         case 9:
-        case 21: 
+        case 21:
+            document.write(`<img src="images/Reloj9.jpg"`);
         break;
-
         case 10:
-        case 22: 
+        case 22:
+            document.write(`<img src="images/Reloj10.jpg"`);
         break;
-
         case 11:
         case 23:
+            document.write(`<img src="images/Reloj11.jpg"`);
         break;
-
         case 12:
         case 24:
+            document.write(`<img src="images/Reloj12.jpg"`);
         break;
 
     }
 }
+
+    var estado = true;
+    var semaforo = prompt("¿De qué color está ahora el semáforo?").toLowerCase();
+    while (estado){
+        if (semaforo != "verde" || semaforo != "rojo" || semaforo != "amarillo"){
+            alert ("Ese color no existe")
+        } else if (semaforo == "verde"){
+            document.write(`<img src="images/semaforoVerde.png"`);
+            estado = false;
+        } else if (semaforo == "rojo"){
+            document.write(`<img src="images/semaforoRojo.png"`);
+            estado = false;
+        }else {
+            document.write(`<img src="images/semaforoNaranja.png"`);
+            estado = false;
+        }      
+    }
+document.write(`</div>`);
 
 /* COCHES */
 
